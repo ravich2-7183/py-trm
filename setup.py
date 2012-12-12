97c9a26ecfaba445c423d5315d7f3aa04a61f12f
Leif Johnson <leif@leifjohnson.net>
2012-12-11 18:07:54 -0600

    Add README.md to package manifest ; update setup.py to reflect best packaging know-how to date.

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 import glob
+import os
 import setuptools
 
 setuptools.setup(
     name='lmj.trm',
-    version='0.1',
+    version='0.1.1',
     namespace_packages=['lmj'],
     packages=setuptools.find_packages(),
+    author='Leif Johnson',
+    author_email='leif@leifjohnson.net',
+    description='Python bindings for the gnuspeech tube resonance model',
+    long_description=open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')).read(),
+    license='MIT',
+    url='http://github.com/lmjohns3/py-trm',
+    keywords=('speech-synthesis '
+              'gnuspeech '
+              'tube-resonance-model'),
     data_files=[('lmj/trm', ['gnuspeech/Monet/diphones.mxml'])],
     ext_modules=[setuptools.Extension(
             '_gnuspeech',
             sources=glob.glob('gnuspeech/Tube/*.c') + ['lmj/trm/gnuspeech_wrap.c'],
             include_dirs=['./gnuspeech'],
             define_macros=[('GNUSTEP', '1')])],
-    author='Leif Johnson',
-    author_email='leif@leifjohnson.net',
-    description='Python bindings for the gnuspeech tube resonance model',
-    long_description=open('README.md').read(),
-    license='MIT',
-    keywords=('speech-synthesis '
-              'gnuspeech '
-              'tube-resonance-model'),
-    url='http://github.com/lmjohns3/py-trm',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
